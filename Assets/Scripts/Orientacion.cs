2165c2b6314879ac1d320e4a81982af988f8a095
VinaryArVr <info@vinarytech.com>
2019-05-30 12:14:58 -0500

    cambios

---
+++ Assets/Scripts/Orientacion.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Orientacion : MonoBehaviour
+{
+    // Start in landscape mode
+    void Start()
+    {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+    }
+}
